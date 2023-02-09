956a3a4936eddd7e57ba80d599d95543c8b6e13e
zhzhahd <y990303@naver.com>
2023-02-09 13:27:38 +0900

    book4

---
+++ main.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+
+    //<!-- Initialize Swiper -->
+
+    var swiper = new Swiper(".mySwiper", {
+        slidesPerView: 3,
+        spaceBetween: 10,
+        loop: true,
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+        },
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
+        },
+      });
+});
